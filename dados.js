b15eb5a03df4f65bfc8a0b1a653a23f89e45a0d5
Giovanna Pennisi <83779918+giovannapennisi@users.noreply.github.com>
2024-09-04 23:00:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,40 @@
+let dados =[
+    {
+        titulo: "Cenoura",
+        descricao: "A cenoura, com sua vibrante cor alaranjada, é muito mais do que um simples legume. Rica em betacaroteno, que nosso corpo transforma em vitamina A, ela é fundamental para a saúde dos olhos, da pele e do sistema imunológico. Além disso, suas fibras contribuem para uma boa digestão e ajudam a controlar o colesterol. Versátil na cozinha, a cenoura pode ser consumida crua, cozida, ralada ou em sucos, adicionando sabor e nutrientes a diversas receitas. Segundo a CEAGESP, a cenoura pode ser encontrada em nossos mercados praticamente o ano todo. No entanto, sua sazonalidade influencia diretamente seu sabor e preço. Durante o inverno, por exemplo, a cenoura costuma apresentar um sabor mais doce e intenso, tornando-a ainda mais apreciada nessa época do ano. Ao escolher suas cenouras, opte por aquelas com cor uniforme, sem manchas e com a raiz firme.",
+        link: "https://ceagesp.gov.br/pesquisar/cenoura"
+    },
+
+    {
+        titulo: "Brócolis",
+        descricao: "O brócolis é um vegetal crucífero rico em nutrientes essenciais, como vitaminas C e K, além de minerais como ferro e potássio. Sua cor verde vibrante indica um alto teor de clorofila, um pigmento com propriedades antioxidantes. O brócolis é uma excelente fonte de fibras, o que auxilia na digestão e promove a sensação de saciedade. Além disso, ele contém sulforafano, um composto com propriedades anticancerígenas. O brócolis pode ser consumido cozido no vapor, refogado ou cru em saladas, adicionando um toque de sabor e cor aos pratos. O brócolis pode ser encontrado em supermercados durante todo o ano, mas sua sazonalidade pode influenciar o preço e a qualidade. As melhores épocas para encontrar brócolis frescos e com sabor mais intenso são a primavera e o outono.",
+        link: "https://ceagesp.gov.br/pesquisar/brocolis"
+    },
+
+    {
+        titulo: "Tomate",
+        descricao: "O tomate é uma fruta rica em licopeno, um pigmento antioxidante que confere a ele sua cor vermelha característica. Além do licopeno, o tomate é uma excelente fonte de vitaminas A e C, potássio e fibras. Sua versatilidade culinária é inegável, podendo ser consumido in natura, em saladas, molhos, sucos e diversas outras preparações. O tomate é um ingrediente fundamental da dieta mediterrânea e está associado à prevenção de diversas doenças, como doenças cardíacas e alguns tipos de câncer. A sazonalidade do tomate varia de acordo com a região, mas em geral, as melhores épocas para encontrar tomates frescos e saborosos são o verão e o outono. Nesses períodos, os tomates tendem a ser mais suculentos e saborosos.",
+        link:"https://ceagesp.gov.br/pesquisar/tomate",
+    },
+
+    {
+        titulo: "Espinafre",
+        descricao: "O espinafre, com suas folhas verdes escuras, é um verdadeiro superalimento. Rico em ferro, cálcio, vitaminas A, C e K, ele fortalece os ossos, aumenta a imunidade e auxilia na produção de hemoglobina. Além disso, suas fibras contribuem para a saciedade e ajudam no controle do peso. O espinafre pode ser consumido cru em saladas, cozido em sopas e refogados, ou ainda adicionado a sucos e smoothies. Para um aproveitamento máximo de seus nutrientes, é recomendado consumir o espinafre cru ou levemente cozido.",
+        link: "https://www.minhavida.com.br/alimentacao/nutricao/espinafre-o-superalimento-que-voce-precisa-conhecer"
+    },
+   
+    {
+        titulo: "Lentilha",
+        descricao: "A lentilha, pequena e nutritiva, é uma leguminosa rica em proteínas vegetais, fibras, ferro e vitaminas do complexo B. Ela ajuda a controlar o colesterol, regula o açúcar no sangue e promove a saciedade. Além disso, a lentilha é uma excelente fonte de energia e pode ser consumida em diversas receitas, como sopas, ensopados, saladas e acompanhamentos. Para um preparo saboroso, é importante deixar as lentilhas de molho em água por algumas horas antes do cozimento.",
+        link: "https://www.tudosobrecozinha.com.br/lentilha/"
+    },
+    {
+        titulo: "Maçã",
+        descricao: "A maçã, fruta versátil e saborosa, é rica em fibras, vitaminas A e C, e possui propriedades antioxidantes. Ela auxilia na digestão, fortalece o sistema imunológico e ajuda a prevenir doenças cardiovasculares. Além disso, a maçã é uma ótima opção para um lanche rápido e nutritivo. Existem diversas variedades de maçã, cada uma com suas características únicas de sabor e textura.",
+        link: "https://www.saude.gov.br/noticias/agencia-saude/46721-maca-uma-fruta-rica-em-nutrientes-e-beneficios-para-saude"
+    }
+];
+
+
+
+
