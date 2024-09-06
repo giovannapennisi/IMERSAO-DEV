6f72d336545f7d3783fe67dc284cf967cfa19519
Giovanna Pennisi <83779918+giovannapennisi@users.noreply.github.com>
2024-09-06 13:33:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ function pesquisar() {
 
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada foi digitado. Tente novamente.</p>.";
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     for (let dado of dados) {
-        resultados += `
+        titulo.toLowerCase();
+        descricao.toLowerCase();
+        tags.toLowerCase();
+
+        if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) 
+         || dado.tags.includes(campoPesquisa))
+        {
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -14,6 +33,14 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }  
     }
+
+    if (!resultados)
+    {
+        resultados = "<p>Nenhum conteúdo foi encontrado.</p>"
+    }
+
     section.innerHTML = resultados;
-} 
+
+ } 
